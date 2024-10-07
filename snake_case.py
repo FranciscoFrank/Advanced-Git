ea1a6a4b91c7f7d59e5859cbf457532d62d9d9b5
Franchesko <m.boichak@edu.internetdevels.com>
2024-10-07 15:15:44 +0300

    Fix the Main.py file

--- snake_case.py
+++ snake_case.py
@@ -1,15 +1,15 @@
+"""
+    Testing Git Hooks 
+"""
 def find_common_elements(seq1, seq2):
-    # Знаходимо спільні елементи
+    """
+        Check if there are identical elements in two given sequences
+    """
     common_elements = set(seq1).intersection(seq2)
-    
-    # Перевіряємо, чи є спільні елементи
     if common_elements:
         print(" ".join(map(str, common_elements)))
     else:
-        prit("Спільні елементи не знайдено")
-
-# Приклад використання
+        print("Спільні елементи не знайдено")
 seq1 = [1, 2, 3, 4]
 seq2 = [3, 4, 5, 6]
-
 find_common_elements(seq1, seq2)
