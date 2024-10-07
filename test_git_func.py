cabb6289502e87d6c0e38621c18007800ca931e7
Franchesko <m.boichak@edu.internetdevels.com>
2024-10-07 15:42:10 +0300

    Added a new function for calculating the type of triangle

---
+++ test_git_func.py
@@ -0,0 +1,47 @@
+"""
+    Testing Git Hooks 
+"""
+def find_common_elements(seq1, seq2):
+    """
+        Check if there are identical elements in two given sequences
+    """
+    common_elements = set(seq1).intersection(seq2)
+    if common_elements:
+        print(" ".join(map(str, common_elements)))
+    else:
+        print("Спільні елементи не знайдено")
+seq1 = [1, 2, 3, 4]
+seq2 = [3, 4 5, 6]
+find_common_elements(seq1, seq2)
+
+def classify_triangle(a, b, c):
+    # Перевіряємо, чи трикутник може існувати
+    if a + b <= c or a + c <= b or b + c <= a:
+        return "Трикутник не може існувати"
+
+    # Визначаємо тип трикутника за сторонами
+    if a == b == c:
+        triangle_type = "рівносторонній"
+    elif a == b or b == c or a == c:
+        triangle_type = "рівнобедрений"
+    else:
+        triangle_type = "різносторонній"
+
+    # Визначаємо тип трикутника за кутами
+    sides_squared = sorted([a**2, b**2, c**2])
+    if sides_squared[0] + sides_squared[1] > sides_squared[2]:
+        angle_type = "гострокутний"
+    elif sides_squared[0] + sides_squared[1] == sides_squared[2]:
+        angle_type = "прямокутний"
+    else:
+        angle_type = "тупокутний"
+
+    return f"Трикутник має сторони: {a}, {b}, {c}\nТрикутник є {triangle_type} і {angle_type}"
+
+# Приклад використання
+a = float(input("Введіть сторону a: "))
+b = float(input("Введіть сторону b: "))
+c = float(input("Введіть сторону c: "))
+
+result = classify_triangle(a, b, c)
+print(result)
